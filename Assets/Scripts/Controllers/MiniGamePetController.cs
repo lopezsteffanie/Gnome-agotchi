f192877bddf9280eff3c97fa614e8f21abd045ca
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-02-09 09:34:33 -0800

    adds horizontal movement for mini game

--- Assets/Scripts/Controllers/MiniGamePetController.cs
+++ Assets/Scripts/Controllers/MiniGamePetController.cs
@@ -1,16 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class MiniGamePetController : MonoBehaviour
 {
+    public float speed, jumpSpeed;
+    private Rigidbody2D r2d;
+
     private void OnEnable()
     {
         GetComponent<PetController>().enabled = false;
+        r2d = GetComponent<Rigidbody2D>();
+        r2d.simulated = true;
     }
 
     private void OnDisable()
     {
         GetComponent<PetController>().enabled = true;
+    }
+
+    private void Update()
+    {
+        CheckMovement();
+    }
+
+    private void CheckMovement()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            r2d.AddForce(new Vector2(speed * Time.deltaTime * Input.GetAxis("Horizontal"),0));
+        }
     }
 }
